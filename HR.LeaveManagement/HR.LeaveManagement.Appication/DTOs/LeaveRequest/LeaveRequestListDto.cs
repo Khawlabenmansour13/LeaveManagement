e65f20daf100378484a88529b5a158c185cb346a
Khawlabenmansour13 <khawla.benmansour@esprit.tn>
2022-06-09 17:28:17 +0100

    firstCommit

---
+++ HR.LeaveManagement/HR.LeaveManagement.Appication/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Appication.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Appication.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+        public LeaveTypeDto DateRequested { get; set; }
+        public bool? Approved { get; set;  }
+    }
+
+}
---
+++ HR.LeaveManagement/HR.LeaveManagement.Appication/Profiles/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Appication.DTOs;
+using HR.LeaveManagement.Appication.DTOs.LeaveRequest;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Appication.MappingProfile 
+{
+    public class MappingProfile : Profile
+    {
+
+      public MappingProfile()
+        { 
+            CreateMap <LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap <LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap <LeaveType, LeaveTypeDto>().ReverseMap();
+
+        }
+    }
+}
